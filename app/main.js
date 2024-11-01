be4ea675c0e54a6f1570ae11f732fdd08242ce04
Zainan <zainanzaher09@gmail.com>
2024-11-01 03:11:18 +0000

    Now can created hashed files using hash-object <flag> <filename.extension>

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib")
+const crypto = require("crypto");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.error("Logs from your program will appear here!");
@@ -16,8 +17,30 @@ switch (command) {
     const hash = process.argv[4];
     catFile(hash);
     break;
+  case "hash-object":
+    createObject();
+
+    break;
   default:
     throw new Error(`Unknown command ${command}`);
+}
+
+function createObject(){
+  // Read contents of the file.
+  const fileName = process.argv[4];
+  const fileContent = fs.readFileSync(fileName);
+  // // Get file size; - destructured size 
+  const content = `blob ${fileContent.length}\x00${fileContent}`;
+  // // Now I will convert the content to sha1 hash - so that we can use it's cryptic name to store data.
+  // // following from before first 2 characters directory name - then remaing 38 subdirectory
+  const hash = crypto.createHash("sha1").update(content).digest("hex");
+  // // now we are gonna create a path (.git/objects/(2characters)/(38characters));
+  const directory = path.join(process.cwd(), ".git", "objects");
+  const parentHashDirectory = path.join(directory, hash.slice(0,2));
+  fs.mkdirSync(parentHashDirectory, { recursive: true });
+  const contentFile = path.join(parentHashDirectory, hash.slice(2));
+  fs.writeFileSync(contentFile, zlib.deflateSync(content));
+  process.stdout.write(hash);
 }
 
 function catFile(hash){
